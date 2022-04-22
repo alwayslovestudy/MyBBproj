7b47480b09fdacff21ece4dd13d9ff3e3eb52116
neo <913144780@qq.com>
2022-04-22 18:35:23 +0800

    遍历句柄表

--- MyBlackBone/MyBB.h
+++ MyBlackBone/MyBB.h
@@ -24,7 +24,7 @@ typedef struct _MMADDRESS_NODE          // 5 elements, 0x28 bytes (sizeof)
 	/*0x010*/     struct _MMADDRESS_NODE* RightChild;
 	/*0x018*/     UINT64       StartingVpn;
 	/*0x020*/     UINT64       EndingVpn;
-}MMADDRESS_NODE, *PMMADDRESS_NODE;
+}MMADDRESS_NODE, * PMMADDRESS_NODE;
 
 typedef struct _MM_AVL_TABLE                          // 6 elements, 0x40 bytes (sizeof) 
 {
@@ -35,9 +35,9 @@ typedef struct _MM_AVL_TABLE                          // 6 elements, 0x40 bytes 
 		/*0x028*/         UINT64       Unused : 3;                      // 5 BitPosition                   
 		/*0x028*/         UINT64       NumberGenericTableElements : 56; // 8 BitPosition                   
 	};
-	/*0x030*/     VOID*        NodeHint;
-	/*0x038*/     VOID*        NodeFreeHint;
-}MM_AVL_TABLE, *PMM_AVL_TABLE;
+	/*0x030*/     VOID* NodeHint;
+	/*0x038*/     VOID* NodeFreeHint;
+}MM_AVL_TABLE, * PMM_AVL_TABLE;
 
 
 union _EX_PUSH_LOCK // Size=8
@@ -51,7 +51,7 @@ union _EX_PUSH_LOCK // Size=8
 		unsigned __int64 Shared : 60; // Size=8 Offset=0 BitOffset=4 BitCount=60
 	};
 	unsigned __int64 Value; // Size=8 Offset=0
-	void * Ptr; // Size=8 Offset=0
+	void* Ptr; // Size=8 Offset=0
 };
 
 
@@ -65,7 +65,7 @@ typedef struct _MMVAD_FLAGS         // 7 elements, 0x8 bytes (sizeof)
 	/*0x000*/     UINT64       Protection : 5;    // 56 BitPosition                 
 	/*0x000*/     UINT64       Spare : 2;         // 61 BitPosition                 
 	/*0x000*/     UINT64       PrivateMemory : 1; // 63 BitPosition                 
-}MMVAD_FLAGS, *PMMVAD_FLAGS;
+}MMVAD_FLAGS, * PMMVAD_FLAGS;
 
 typedef struct _MMVAD_FLAGS2          // 9 elements, 0x4 bytes (sizeof) 
 {
@@ -78,7 +78,7 @@ typedef struct _MMVAD_FLAGS2          // 9 elements, 0x4 bytes (sizeof)
 	/*0x000*/     UINT32       ExtendableFile : 1;  // 29 BitPosition                 
 	/*0x000*/     UINT32       Inherit : 1;         // 30 BitPosition                 
 	/*0x000*/     UINT32       CopyOnWrite : 1;     // 31 BitPosition                 
-}MMVAD_FLAGS2, *PMMVAD_FLAGS2;
+}MMVAD_FLAGS2, * PMMVAD_FLAGS2;
 
 typedef struct _MMVAD_FLAGS3              // 8 elements, 0x8 bytes (sizeof) 
 {
@@ -90,7 +90,7 @@ typedef struct _MMVAD_FLAGS3              // 8 elements, 0x8 bytes (sizeof)
 	/*0x000*/     UINT64       Spare2 : 8;              // 24 BitPosition                 
 	/*0x000*/     UINT64       LargePageCreating : 1;   // 32 BitPosition                 
 	/*0x000*/     UINT64       Spare3 : 31;             // 33 BitPosition                 
-}MMVAD_FLAGS3, *PMMVAD_FLAGS3;
+}MMVAD_FLAGS3, * PMMVAD_FLAGS3;
 
 typedef struct _MMVAD                          // 15 elements, 0x78 bytes (sizeof) 
 {
@@ -128,22 +128,75 @@ typedef struct _MMVAD                          // 15 elements, 0x78 bytes (sizeo
 	/*0x058*/     struct _MMPTE* LastContiguousPte;
 	/*0x060*/     struct _LIST_ENTRY ViewLinks;              // 2 elements, 0x10 bytes (sizeof)  
 	/*0x070*/     struct _EPROCESS* VadsProcess;
-}MMVAD, *PMMVAD;
+}MMVAD, * PMMVAD;
 
 
+typedef union _EXHANDLE
+{
+	struct
+	{
+		int TagBits : 2;
+		int Index : 30;
+	} u;
+	void* GenericHandleOverlay;
+	ULONG_PTR Value;
+} EXHANDLE, * PEXHANDLE;
 
+typedef struct _HANDLE_TABLE
+{
+	ULONG_PTR TableCode;
+	struct _EPROCESS* QuotaProcess;
+	HANDLE UniqueProcessId;
+	void* HandleLock;
+	struct _LIST_ENTRY HandleTableList;
+	EX_PUSH_LOCK HandleContentionEvent;
+	struct _HANDLE_TRACE_DEBUG_INFO* DebugInfo;
+	int ExtraInfoPages;
+	ULONG Flags;
+	ULONG FirstFreeHandle;
+	struct _HANDLE_TABLE_ENTRY* LastFreeHandleEntry;
+	ULONG HandleCount;
+	ULONG NextHandleNeedingPool;
+	// More fields here...
+} HANDLE_TABLE, * PHANDLE_TABLE;
+typedef struct _HANDLE_TABLE_ENTRY // Size=16
+{
+	union
+	{
+		ULONG_PTR VolatileLowValue; // Size=8 Offset=0
+		ULONG_PTR LowValue; // Size=8 Offset=0
+		struct _HANDLE_TABLE_ENTRY_INFO* InfoTable; // Size=8 Offset=0
+		struct
+		{
+			ULONG_PTR Unlocked : 1; // Size=8 Offset=0 BitOffset=0 BitCount=1
+			ULONG_PTR RefCnt : 16; // Size=8 Offset=0 BitOffset=1 BitCount=16
+			ULONG_PTR Attributes : 3; // Size=8 Offset=0 BitOffset=17 BitCount=3
+			ULONG_PTR ObjectPointerBits : 44; // Size=8 Offset=0 BitOffset=20 BitCount=44
+		};
+	};
+	union
+	{
+		ULONG_PTR HighValue; // Size=8 Offset=8
+		struct _HANDLE_TABLE_ENTRY* NextFreeHandleEntry; // Size=8 Offset=8
+		union _EXHANDLE LeafHandleValue; // Size=8 Offset=8
+		struct
+		{
+			ULONG GrantedAccessBits : 25; // Size=4 Offset=8 BitOffset=0 BitCount=25
+			ULONG NoRightsUpgrade : 1; // Size=4 Offset=8 BitOffset=25 BitCount=1
+			ULONG Spare : 6; // Size=4 Offset=8 BitOffset=26 BitCount=6
+		};
+	};
+	ULONG TypeInfo; // Size=4 Offset=12
+} HANDLE_TABLE_ENTRY, * PHANDLE_TABLE_ENTRY;
 
-
-
-
-
-
-
-
-
-
-
-
+typedef BOOLEAN(*EX_ENUMERATE_HANDLE_ROUTINE)(
+#if !defined(_WIN7_)
+	IN PHANDLE_TABLE HandleTable,
+#endif
+	IN PHANDLE_TABLE_ENTRY HandleTableEntry,
+	IN HANDLE Handle,
+	IN PVOID EnumParameter
+	);
 
 
 
--- MyBlackBone/main.c
+++ MyBlackBone/main.c
@@ -7,8 +7,10 @@
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath);
 NTSTATUS DispatchIO(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
 NTSTATUS TraverseVAD(PEPROCESS pEProcess);
+NTSTATUS EnumProcessHandleTable(ULONG procPid);
 void  DriverUnload(PDRIVER_OBJECT pDriverObject);
 VOID CreateProcessNotify(IN HANDLE ParentId, IN HANDLE ProcessId, IN BOOLEAN Create);
+NTSTATUS EnumProcessHandleTable(ULONG Pid);
 #pragma alloc_text(INIT, DriverEntry)
 
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
@@ -24,7 +26,7 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING Registry
 	status = IoCreateDevice(DriverObject, 0, &deviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &deviceObject);
 	if (!NT_SUCCESS(status))
 	{
-		KdPrint(("Create Device Faield err：%x\n", status));
+		KdPrint(("Create Device Faildd err：%x\n", status));
 		return status;
 	}
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = DispatchIO;
@@ -37,11 +39,11 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING Registry
 	status = IoCreateSymbolicLink(&symbol_Link_Name, &deviceName);
 	if (!NT_SUCCESS(status))
 	{
-		KdPrint(("Create Symlnkname Faield err：%x\n", status));
+		KdPrint(("Create Symlnkname Failed err：%x\n", status));
 		IoDeleteDevice(deviceObject);
 		return status;
 	}
-	HANDLE hPid = (HANDLE)1012;
+	/*HANDLE hPid = (HANDLE)1012;
 	PEPROCESS pProcess;
 	status = PsLookupProcessByProcessId(hPid, &pProcess);
 	if (!NT_SUCCESS(status))
@@ -51,11 +53,12 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING Registry
 	}
 
 	TraverseVAD(pProcess);
-
-
 	KdPrint(("Driver Entry Exit\n"));
 	if (pProcess)
-		ObDereferenceObject(pProcess);
+		ObDereferenceObject(pProcess);*/
+		//枚举进程句柄表
+	ULONG Pid = 1108;
+	EnumProcessHandleTable(Pid);
 	return status;
 
 
@@ -87,18 +90,9 @@ NTSTATUS DispatchIO(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
 		}
 
 
-
-
-
 	}
 
-
-
 	}
-
-
-
-
 
 
 	Irp->IoStatus.Status = status;
@@ -122,8 +116,6 @@ VOID CreateProcessNotify(IN HANDLE ParentId, IN HANDLE ProcessId, IN BOOLEAN Cre
 		KdPrint((" PID: %x Create\n", ProcessId));
 	else
 		KdPrint((" PID: %x Terminate\n", ProcessId));
-
-
 }
 //遍历二叉树
 NTSTATUS TraverseTree(PMMVAD pRoot)
@@ -152,8 +144,65 @@ NTSTATUS TraverseVAD(PEPROCESS pEProcess)
 	KdPrint(("vadroot:%llx\n", pRoot));
 	TraverseTree((PMMVAD)pRoot);
 
-	
 }
+
+
+typedef NTKERNELAPI
+BOOLEAN(*_ExEnumHandleTable)(
+	IN PHANDLE_TABLE,
+	IN EX_ENUMERATE_HANDLE_ROUTINE,
+	IN PVOID,
+	OUT PHANDLE
+	);
+
+
+BOOLEAN MBHandleCallback(
+	IN PHANDLE_TABLE_ENTRY HandleTableEntry,
+	IN HANDLE Handle,
+	IN PVOID EnumParameter
+)
+{
+	KdPrint(("Handle:%x\n", Handle));
+	KdPrint(("HandleTableEntry:%llx\n", HandleTableEntry));
+	if (Handle == 0x38)
+		return TRUE;
+
+
+}
+
+NTSTATUS EnumProcessHandleTable(ULONG Pid)
+{
+	PEPROCESS pProcess;
+	NTSTATUS status = STATUS_SUCCESS;
+	status = PsLookupProcessByProcessId((HANDLE)Pid, &pProcess);
+	if (NT_SUCCESS(status))
+	{
+		UNICODE_STRING uFuncName;
+		RtlInitUnicodeString(&uFuncName, L"ExEnumHandleTable");
+		_ExEnumHandleTable ExEnumHandleTable = MmGetSystemRoutineAddress(&uFuncName);
+		if (ExEnumHandleTable)
+		{
+			KdPrint(("get ExEnumHandleTable success\n"));
+			PHANDLE_TABLE handleTable = *(PHANDLE_TABLE*)(PUCHAR)pProcess + 0x200;
+			KdPrint(("handleTable:%llx", handleTable));
+			LONG para = 0;
+			BOOLEAN ret = ExEnumHandleTable(handleTable, &MBHandleCallback, &para, NULL);
+			KdPrint(("ret:%x\n", ret));
+			return status;
+		}
+	}
+	status = STATUS_INVALID_ADDRESS;
+	return status;
+
+
+
+
+
+
+
+
+}
+
 
 
 
